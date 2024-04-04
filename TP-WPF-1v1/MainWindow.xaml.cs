23d87e0f72ca0d5d67f09ef07b0bd9e14a6b1212
mathias <mathiasdumaspro@gmail.com>
2024-04-04 13:25:59 +0200

    databing, fight1v1

---
+++ TP-WPF-1v1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP_WPF_1v1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Opponent Raton { get; set; }
+        public Opponent Roger { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Raton = new Opponent("Le roi des ratons maudit");
+            Roger = new Opponent("Roger, le gardien du parc");
+            DataContext = this;
+        
+        }
+
+        private void Raton_Attack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (SomeoneIsDead())
+            {
+                result.Text = "Le combat est terminé !";
+            } else
+            {
+                Roger.HealthPoints -= 10;
+                if (!IsDead(Roger))
+                {
+                    botte.Visibility = Visibility.Hidden;
+                    result.Text = "Raton griffe Roger !";
+                    griffe.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    result.Text = "Roger est MORT !";
+                }
+            }       
+        }
+
+        private void Roger_Attack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (SomeoneIsDead())
+            {
+                result.Text = "Le combat est terminé !";
+            }
+            else
+            {
+                Raton.HealthPoints -= 10;
+                if (!IsDead(Raton))
+                {
+                    griffe.Visibility = Visibility.Hidden;
+                    result.Text = "Roger savate Raton !";
+                    botte.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    result.Text = "Raton est MORT !";
+                }
+            }
+            
+        }
+
+        private bool IsDead(Opponent opponent)
+        {
+            return opponent.HealthPoints <= 0;
+        }
+
+        private bool SomeoneIsDead()
+        {
+            return Raton.HealthPoints <= 0 || Roger.HealthPoints <= 0;
+        }
+    }
+}
---
+++ TP-WPF-1v1/Opponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_WPF_1v1
+{
+    public class Opponent : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        private int healthPoints;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int HealthPoints
+        {
+            get { return healthPoints; }
+            set
+            {
+                if (healthPoints != value)
+                {
+                    healthPoints = value;
+                    NotifyPropertyChanged("HealthPoints");
+                }
+            }
+        }
+
+        public Opponent(string Name)
+        {
+            this.Name = Name;
+            healthPoints = 100;
+        }
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
